fd8cec94722fd1a96653bc105ed02969bd3fb04e
고태진 <thereisnotruth12@gmail.com>
2023-01-18 09:08:36 +0900

    Feature: '[FE] 헤더 추가'
    App에서 불필요한 요소를 지우고 Header 추가
    
    [#22]

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+import { Button } from '@mui/material';
+
+import '../styles/components/header.scss';
+import logo from '../assets/logo.png';
+
+const Header = () => {
+  return <header>헤더</header>;
+};
+
+export default Header;
