bdd5a8e2219e7b5f820f93901791378fd3b7e0fe
고태진 <thereisnotruth12@gmail.com>
2023-01-18 17:36:37 +0900

    Feat: '[FE] 헤더 검색창 추가'
    헤더에 검색창 추가
    
    [#22]

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 
 import '../styles/components/header.scss';
 import logo from '../assets/logo.png';
 
 const Header = () => {
+  const searchbarRef = useRef(null); // 검색창을 접근/제어하기 위한 hook
+
+  // form 값 제출시 실행할 함수
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // 새로고침 방지
+    if (searchbarRef?.current) {
+      // searchbarRef.currnet의 타입이 HTMLInputElement라고 컴파일러에게 알려줌
+      const target = searchbarRef.current as HTMLInputElement;
+
+      // 검색 로직
+      console.log(target.value);
+
+      // 검색창을 빈칸으로
+      target.value = '';
+    }
+  };
+
   return (
     <header>
       {/* 로고 */}
@@ -25,6 +42,14 @@ const Header = () => {
           <li className="nav-item">자랑 게시판</li>
         </Link>
       </ul>
+
+      {/* 검색창 */}
+      <form className="search" onSubmit={handleSearchSubmit}>
+        <input type="text" className="searchbar" ref={searchbarRef} />
+        <button type="submit" className="search-button">
+          <SearchIcon />
+        </button>
+      </form>
     </header>
   );
 };
