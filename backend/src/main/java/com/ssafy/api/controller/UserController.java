8457172cb9ee08a04caf3b9c79fd3b4148f8785d
Son Jeong Hun <thswjdgns9@naver.com>
2023-01-20 14:34:18 +0900

    Feature: [BE] 유저 컨트롤러 이메일 중복 확인 코드 추가 #140

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.ssafy.api.controller;
 
+import com.ssafy.api.request.UserNicknameCheckGetReq;
+import com.ssafy.api.response.UserEmailCheckGetRes;
+import com.ssafy.api.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -22,6 +25,9 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import springfox.documentation.annotations.ApiIgnore;
+
+import javax.print.DocFlavor;
+import javax.swing.text.StyledEditorKit;
 
 /**
  * 유저 관련 API 요청 처리를 위한 컨트롤러 정의.
@@ -69,5 +75,23 @@ public class UserController {
 		User user = userService.getUserByAuth(email);
 		
 		return ResponseEntity.status(200).body(UserRes.of(user));
+	}
+
+	@GetMapping("/email-check")
+	@ApiOperation(value = "이메일 중복 체크", notes = "DB에 이미 email이 있는지 체크")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "성공"),
+			@ApiResponse(code = 401, message = "인증 실패"),
+			@ApiResponse(code = 404, message = "사용자 없음"),
+			@ApiResponse(code = 500, message = "서버 오류")
+	})
+	public ResponseEntity<Boolean> getUserEmail(@RequestBody UserNicknameCheckGetReq userNicknameCheckGetReq){
+
+		if(userService.getUserByNickname(userNicknameCheckGetReq.getNickname()) == null){
+			return ResponseEntity.status(200).body(true);
+		}
+		else{
+			return ResponseEntity.status(200).body(false);
+		}
 	}
 }
