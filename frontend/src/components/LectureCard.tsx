67fedd35129e12acd9dc043e96c2d4f1896057f8
김진호 <wlsghsla94@naver.com>
2023-01-25 01:46:30 +0900

    Feat: [FE] '메인페이지 추천강의 카드 구현'
    LectureCard.tsx component 작성, dummy data 사용
    
    [#37]

---
+++ frontend/src/components/LectureCard.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import api, { Lecture } from './api';
+import logo from '../assets/logo.png';
+
+// interface Props {}
+
+// dummy data
+const LectureCard = () => {
+  const dummyData = [
+    { id: 1, image: 'image1.jpg', instructor: 'John Doe', name: 'Introduction to React', rating: 4.5 },
+    { id: 2, image: 'image2.jpg', instructor: 'Jane Smith', name: 'Advanced JavaScript', rating: 4.0 },
+    { id: 3, image: 'image3.jpg', instructor: 'Bob Johnson', name: 'Node.js for Beginners', rating: 3.5 },
+  ];
+
+  const [lectures, setLectures] = useState<Lecture[]>(dummyData);
+  return (
+    <div className="lecture">
+      {lectures.map((lecture) => (
+        <div className="lecture-card" key={lecture.id}>
+          <img src={logo} alt={lecture.name} />
+          <p className="instructor">Instructor: {lecture.instructor}</p>
+          <p className="name">Lecture Name: {lecture.name}</p>
+          <p className="rating">Rating: {lecture.rating}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LectureCard;
+
+// DB API로 data 받아올때 코드
+// import React, { useEffect, useState } from 'react';
+// import api from './api';
+
+// interface Props {}
+
+// const LecturePage: React.FC<Props> = () => {
+//   const [lectures, setLectures] = useState<Lecture[]>([]);
+
+//   useEffect(() => {
+//     api.getLectures((error, results) => {
+//       if (error) throw error;
+//       setLectures(results);
+//     });
+//   }, []);
+
+//   return (
+//     <div>
+//       {lectures.map((lecture) => (
+//         <div key={lecture.id}>
+//           <img src={lecture.image} alt={lecture.name} />
+//           <p>Instructor: {lecture.instructor}</p>
+//           <p>Lecture Name: {lecture.name}</p>
+//           <p>Rating: {lecture.rating}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default LecturePage;
