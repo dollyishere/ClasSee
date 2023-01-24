389aa5f7994581c6a3d06e4c38631fb03d94cea5
김진호 <wlsghsla94@naver.com>
2023-01-25 01:52:45 +0900

    Feat: '[FE] 메인페이지 추천강의 카드 구현'
    MainPage.tsx에 <LectureCard />, <Lizard /> 추가
    
    [#37]

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import CustomCarousel from '../components/CustomCarousel';
+import LectureCard from '../components/LectureCard';
+import Lizard from '../components/Lizard';
 
 const MainPage = () => {
   return (
     <div>
       <CustomCarousel />
+      <LectureCard />
+      <Lizard />
     </div>
   );
 };
