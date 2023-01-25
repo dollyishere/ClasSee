4a373ca6e89965c221e582f295b300f14b9a016f
Son Jeong Hun <thswjdgns9@naver.com>
2023-01-25 14:09:24 +0900

    [Feature]: [BE] 공지사항 create 관련 서비스 및 기능 추가

---
+++ backend/src/main/java/com/ssafy/api/service/NoticeService.java
@@ -0,0 +1,12 @@
+package com.ssafy.api.service;
+
+import com.ssafy.api.request.NoticeCreatePostReq;
+
+public interface NoticeService {
+
+    void createNotice(NoticeCreatePostReq noticeCreatePostReq) throws Exception;
+    void readNotice();
+    void updateNotice();
+    void deleteNotice();
+
+}
