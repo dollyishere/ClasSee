17080f5f01b62f7ced7e95c152de010321b0727a
dollyishere <rundollyrun8@gmail.com>
2023-01-25 17:55:31 +0900

    Feat: '[FE] 이미지 파일 upload 일부 구현' 이미지 파일 upload 기능 계속 구현 중에 있음 [#48]

--- frontend/src/components/open_lecture_steps/StepTwo.tsx
+++ frontend/src/components/open_lecture_steps/StepTwo.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Box from '@mui/material/Box';
@@ -9,23 +9,60 @@ import CardActions from '@mui/material/CardActions';
 import TextField from '@mui/material/TextField';
 import Menu, { MenuProps } from '@mui/material/Menu';
 
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+
+interface Props {
+  limit?: number;
+}
+
 const StepTwo = () => {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  const onUploadImageButtonClick = useCallback(() => {
-    if (!inputRef.current) {
-      return;
+  const [showImages, setShowImages] = useState([]);
+  // const [imageUrlLists, setimageUrlLists] = useState([]);
+
+  // 이미지 상대경로 저장
+  const handleAddImages = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const imageLists = e.target.files as FileList;
+    let imageUrlLists = [...showImages] as string[];
+    console.log(showImages);
+    console.log(imageLists);
+    console.log(imageLists.length);
+
+    for (let i = 0; i < imageLists.length; i += 1) {
+      const currentImageUrl = URL.createObjectURL(imageLists[i]);
+      console.log(currentImageUrl);
+      imageUrlLists.push(currentImageUrl);
+      console.log(imageUrlLists);
     }
-    inputRef.current.click();
-  }, []);
+
+    if (imageUrlLists.length > 10) {
+      imageUrlLists = imageUrlLists.slice(0, 10);
+    }
+    console.log(imageUrlLists.type);
+    setShowImages(imageUrlLists);
+  };
+
+  // X버튼 클릭 시 이미지 삭제
+  const handleDeleteImage = (id: number) => {
+    setShowImages(showImages.filter((_, index) => index !== id));
+  };
+
   return (
     <div>
       <h2>Step 2. 소개 사진 등록</h2>
-      <form>
-        <input hidden accept="image/*" multiple type="file" ref={inputRef} id="upload-photo-input" />
-        <button type="button" id="upload-photo-btn" onClick={onUploadImageButtonClick}>
-          +
-        </button>
-      </form>
+      <div>
+        <label htmlFor="input-file">
+          <input hidden type="file" id="input-file" multiple onChange={handleAddImages} />
+          <AddCircleOutlineIcon fill="#646F7C" />
+        </label>
+        {/* 저장해둔 이미지들을 순회하면서 화면에 이미지 출력 */}
+        {/* {showImages.map((image: Blob, id: Number) => (
+          <div key={id}>
+            <img src={image} alt={`${image}-${id}`} />
+            <RemoveCircleOutlineIcon onClick={() => handleDeleteImage(id)} />
+          </div>
+        ))} */}
+      </div>
       <CardActions>
         <Link to="/open_lecture/1">
           <Button type="submit" variant="contained">
