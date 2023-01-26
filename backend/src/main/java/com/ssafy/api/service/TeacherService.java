0e7e44034e91b4202feb01f974e46c6cd0b4f55f
7unho <rlawnsgh8395@naver.com>
2023-01-26 16:56:45 +0900

    Feat: [BE] 개설 강의 목록 조회 api 구현 사용자가 개설한 강의의 목록 조회 구현, 북마크 관련 기능 추가 필요

--- backend/src/main/java/com/ssafy/api/service/TeacherService.java
+++ backend/src/main/java/com/ssafy/api/service/TeacherService.java
@@ -1,4 +1,10 @@
 package com.ssafy.api.service;
 
+import com.ssafy.db.entity.lesson.Lesson;
+import com.ssafy.db.entity.user.User;
+
+import java.util.List;
+
 public interface TeacherService {
+    List<Lesson> getLessonList(User user);
 }
