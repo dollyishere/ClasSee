755b17be6be062b4c75625cc673c5f57af9e936f
Son Jeong Hun <thswjdgns9@naver.com>
2023-01-26 14:47:41 +0900

    [Feat]: [BE] 게시글 삭제를 위한 컨트롤러 기능 추가

--- backend/src/main/java/com/ssafy/api/controller/ArticleController.java
+++ backend/src/main/java/com/ssafy/api/controller/ArticleController.java
@@ -10,10 +10,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(value = "게시글 API", tags = {"Article"})
 @RestController
@@ -26,14 +23,31 @@ public class ArticleController {
     @PostMapping()
     @ApiOperation(value = "게시글 등록", notes = "작성자 이메일, 제목, 내용, 이미지 주소 입력 후 게시글 생성")
     @ApiResponses({
-            @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 401, message = "권한이 없음"),
+            @ApiResponse(code = 200, message = "성공")
     })
-    public ResponseEntity<? extends BaseResponseBody> register(@RequestBody ArticleRegisterPostReq articleRegisterPostReq) {
+    public ResponseEntity<? extends BaseResponseBody> registeArticle(@RequestBody ArticleRegisterPostReq articleRegisterPostReq) {
 
         articleService.createArticle(articleRegisterPostReq);
 
         return ResponseEntity.status(200).body(BaseResponseBody.of(200,"등록 성공"));
+
+    }
+
+    @DeleteMapping()
+    @ApiOperation(value = "게시글 삭제", notes = "삭제하려는 사람과 게시글 아이디를 받아, 권한이 있는지 확인 후 삭제")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "삭제 권한이 없음")
+    })
+    public ResponseEntity<? extends BaseResponseBody> deleteArticle(@RequestParam String email, @RequestParam Long id) {
+
+        try {
+            articleService.deleteArticle(email, id);
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body(BaseResponseBody.of(401, "작성자와 다릅니다"));
+        }
+
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200,"삭제 성공"));
 
     }
 
