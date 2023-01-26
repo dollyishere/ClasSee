011df20b2289bb14b639746ae1cca349a95eee6a
Son Jeong Hun <thswjdgns9@naver.com>
2023-01-26 13:47:00 +0900

    [Feat]: [BE] 게시물 생성을 위한 서비스 기능 추가

---
+++ backend/src/main/java/com/ssafy/api/service/ArticleServiceImpl.java
@@ -0,0 +1,55 @@
+package com.ssafy.api.service;
+
+import com.ssafy.api.request.ArticleRegisterPostReq;
+import com.ssafy.db.entity.board.Article;
+import com.ssafy.db.entity.user.User;
+import com.ssafy.db.repository.ArticleRepositorySupport;
+import com.ssafy.db.repository.UserRepositorySupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service("articleService")
+@Transactional
+public class ArticleServiceImpl implements ArticleService{
+
+    @Autowired
+    ArticleRepositorySupport articleRepositorySupport;
+    @Autowired
+    UserRepositorySupport userRepositorySupport;
+
+
+    @Override
+    public void createArticle(ArticleRegisterPostReq articleRegisterPostReq) {
+        User user = userRepositorySupport
+                .findUserByAuth(articleRegisterPostReq.getEmail())
+                .get();
+
+        Article article = Article.builder()
+                .title(articleRegisterPostReq.getTitle())
+                .content(articleRegisterPostReq.getContent())
+                .img(articleRegisterPostReq.getImg())
+                .hit(0L)
+                .user(user)
+                .build();
+
+        articleRepositorySupport.save(article);
+    }
+
+    @Override
+    public Article readArticle() {
+        return null;
+    }
+
+    @Override
+    public void updateArticle() {
+
+    }
+
+    @Override
+    public void deleteArticle() {
+
+    }
+}
