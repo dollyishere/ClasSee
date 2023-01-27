175635961f6c223eab8f34b4746aa1f8acc294f3
No88888888 <isagkim@gmail.com>
2023-01-27 16:50:28 +0900

    Feat: '[FE] App에 RecoilRoot 추가'
    컴포넌트에 Recoil 사용으로 인한 app 컴포넌트에 RecoilRoot 추가
    
    [#232]

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-
+import { RecoilRoot } from 'recoil';
 import './App.css';
 import './styles/main.scss';
 
@@ -12,13 +12,15 @@ import SignUpPage from './pages/SignUpPage';
 const App = () => {
   const location = useLocation();
   return (
-    <div className="App">
-      {location.pathname === '/signup' ? '' : <Header />}
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        {location.pathname === '/signup' ? '' : <Header />}
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
+    </RecoilRoot>
   );
 };
 
