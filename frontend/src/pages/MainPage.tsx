b0e268d6c5193c991247eeb0d014f9dbe1a3d991
No88888888 <isagkim@gmail.com>
2023-01-27 16:47:53 +0900

    Feat: '[FE] mainpage에 개설강의, 신청 강의 추가'
    useRecoilValue를 이용하여 authToken을 받아와 값이 있다면
    개설강의 부분과 신청강의 부분을 보여주고
    값이 없다면
    해당 부분들을 보여주지 않는다
    
    [#232]

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import authToken from '../store/authToken';
 import CustomCarousel from '../components/CustomCarousel';
 import RecommandLessons from '../components/RecommandLessons';
+import CreateLessons from '../components/CreateLessons';
+import ApplyLessons from '../components/ApplyLessons';
 
 const MainPage = () => {
+  const authTokenState = useRecoilValue(authToken);
+
   return (
     <div className="page">
       <CustomCarousel />
+      {authTokenState ? <CreateLessons /> : <p>no lessons</p>}
+      {authTokenState ? <ApplyLessons /> : <p>no lessons</p>}
       <RecommandLessons />
     </div>
   );
