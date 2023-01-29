1a47fa9b20df8f03ffb22a231819275088841753
Son Jeong Hun <thswjdgns9@naver.com>
2023-01-30 08:55:53 +0900

    [Feat]: [BE] qna 서비스 추가

---
+++ backend/src/main/java/com/ssafy/api/service/QnAServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ssafy.api.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("QnAService")
+@Transactional
+public class QnAServiceImpl implements QnAService {
+}
