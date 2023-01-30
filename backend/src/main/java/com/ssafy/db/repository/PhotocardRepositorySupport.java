ffa76c2b33421dabf6ebcf8a68e087c578e3a607
Son Jeong Hun <thswjdgns9@naver.com>
2023-01-30 17:44:02 +0900

    [Feat]: [BE] 포토카드 리포지토리 기능 추가

---
+++ backend/src/main/java/com/ssafy/db/repository/PhotocardRepositorySupport.java
@@ -0,0 +1,50 @@
+package com.ssafy.db.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.db.entity.board.Photocard;
+import com.ssafy.db.entity.board.QPhotocard;
+import com.ssafy.db.entity.user.QUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PhotocardRepositorySupport {
+
+    private final EntityManager em;
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    QPhotocard qPhotocard = QPhotocard.photocard;
+
+    QUser qUser = QUser.user;
+
+    public void save(Photocard photocard) { em.persist(photocard); }
+
+    public void delete(Photocard photocard) { em.remove(photocard); }
+
+    public Photocard findOne(Long id) { return em.find(Photocard.class, id); }
+
+    public List<Photocard> findAll() {
+        return jpaQueryFactory
+                .select(qPhotocard)
+                .from(qPhotocard)
+                .fetch();
+    }
+
+    public List<Photocard> findList(int offset, int limit, Long user_id) {
+        return jpaQueryFactory
+                .selectFrom(qPhotocard)
+                .where(qPhotocard.user.id.eq(user_id))
+                .orderBy(qPhotocard.id.desc())
+                .offset(offset)
+                .limit(limit)
+                .fetch();
+    }
+
+
+
+}
