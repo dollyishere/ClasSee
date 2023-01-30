ea1c7ddd710d7ad96d95b101681ea8df61e633ed
Son Jeong Hun <thswjdgns9@naver.com>
2023-01-30 14:26:05 +0900

    [Feat]: [BE] QnA 컨트롤러 업데이트 기능 추가

--- backend/src/main/java/com/ssafy/api/controller/QnaController.java
+++ backend/src/main/java/com/ssafy/api/controller/QnaController.java
@@ -1,8 +1,15 @@
 package com.ssafy.api.controller;
 
 import com.ssafy.api.request.QnaRegisterPostReq;
+import com.ssafy.api.request.QnaUpdatePutReq;
+import com.ssafy.api.response.NoticeInfoRes;
+import com.ssafy.api.response.PageGetRes;
+import com.ssafy.api.response.QnaInfoGetRes;
+import com.ssafy.api.response.QnaListGetRes;
 import com.ssafy.api.service.QnaService;
 import com.ssafy.common.model.response.BaseResponseBody;
+import com.ssafy.db.entity.board.Notice;
+import com.ssafy.db.entity.qna.Qna;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -10,6 +17,9 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Api(value = "QnA API", tags = {"QnA"})
 @RestController
@@ -28,25 +38,78 @@ public class QnaController {
 
         qnaService.createQna(qnaRegisterPostReq);
 
-        return ResponseEntity.status(200).body(BaseResponseBody.of(200,"success"));
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200,"성공"));
     }
 
     @DeleteMapping()
     @ApiOperation(value = "Qna 삭제", notes = "Qna 아이디와 로그인한 사람의 이메일을 받아, 비교한 뒤 삭제")
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 401, message = "실패")
+            @ApiResponse(code = 401, message = "권한이 없음")
     })
     public ResponseEntity<? extends BaseResponseBody> deleteQna(@RequestParam String email, @RequestParam Long id) throws Exception {
 
         try{
             qnaService.deleteQna(email, id);
         } catch (Exception e){
-            return ResponseEntity.status(401).body(BaseResponseBody.of(401,"fail"));
+            return ResponseEntity.status(401).body(BaseResponseBody.of(401,"권한이 없음"));
         }
 
-        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "success"));
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "성공"));
     }
+
+    @GetMapping()
+    @ApiOperation(value = "qna 상세 조회", notes = "qna를 클릭했을 때, qna id를 입력받아 공지 상세정보 반환")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공")
+    })
+    public ResponseEntity<?> getQnaInfo(@RequestParam Long id){
+
+        Qna qna = qnaService.readQna(id);
+        QnaInfoGetRes qnaInfoGetRes = new QnaInfoGetRes(qna);
+
+        return ResponseEntity.status(200).body(qnaInfoGetRes);
+    }
+
+    @GetMapping("/list")
+    @ApiOperation(value = "qna 리스트 조회", notes = "내가 한 qna 리스트 반환")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공")
+    })
+    public ResponseEntity<?> getQnaList(@RequestParam int offset, @RequestParam int limit, @RequestParam String email){
+
+        List<Qna> qnaList = qnaService.readQnaList(offset, limit, email);
+        List<QnaListGetRes> qnaListGetResList = qnaList
+                .stream()
+                .map(q -> new QnaListGetRes(q)).collect(Collectors.toList());
+        Long qnaCount = qnaService.qnaCount();
+
+        PageGetRes qnaPage = new PageGetRes();
+        qnaPage.setCount(qnaCount);
+        qnaPage.setPage(qnaListGetResList);
+
+        return ResponseEntity.status(200).body(qnaPage);
+
+    }
+
+    @PutMapping()
+    @ApiOperation(value = "qna 수정", notes = "내가 한 qna 수정")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "권한이 없음")
+    })
+    public ResponseEntity<? extends BaseResponseBody> updateQna(@RequestBody QnaUpdatePutReq qnaUpdatePutReq) {
+
+        try{
+            qnaService.updateQna(qnaUpdatePutReq);
+        } catch (Exception e){
+            return ResponseEntity.status(401).body(BaseResponseBody.of(401,"권한이 없음"));
+        }
+
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200,"성공"));
+
+    }
+
 
 
 }
