ba09bdc8d5c49da715a0e3816c68da39b182efb5
dollyishere <rundollyrun8@gmail.com>
2023-01-31 10:48:45 +0900

    refactor: '[FE] 페이지 이동 버튼 생성 및 세부 컴포넌트 import' 강의페이지 component간 전환 방식이 변경되면서 이동 버튼 생성 및 렌더링할 세부 컴포넌트를 import, selectedComponent 값으로 렌더링 여부를 제어함 [#239]

--- frontend/src/pages/CreateLessonPage.tsx
+++ frontend/src/pages/CreateLessonPage.tsx
@@ -1,20 +1,49 @@
-import React, { useRef } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { useState, useRef, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/pages/_create-lesson-page.scss';
 
-import Card from '@mui/material/Card';
+import { Button, Card, CardActions } from '@mui/material';
+
+import StepOne from '../components/create-lesson-steps/StepOne';
+import StepTwo from '../components/create-lesson-steps/StepTwo';
+import StepThree from '../components/create-lesson-steps/StepThree';
+import StepFour from '../components/create-lesson-steps/StepFour';
+import StepFive from '../components/create-lesson-steps/StepFive';
+import StepSix from '../components/create-lesson-steps/StepSix';
 
 const CreateLessonPage = () => {
-  const [progress, setProgress] = React.useState(10);
+  const [selectedComponent, setSelectedComponent] = useState(1);
+  const [data, setData] = useState<string>('');
   return (
     <div className="container">
       {/* 페이지 제목 지정 */}
       <h1>강의 간편 개설하기</h1>
       {/* 카드로 form이 들어갈 영역 지정 */}
+      {/* selectedComponent 값이 변환될 시, 해당하는 컴포넌트를 리렌더링함 */}
       <Card sx={{ minWidth: 275 }}>
-        {/* 각 자식 component가 렌더링 되는 위치를 Outlet으로 지정 */}
-        <Outlet />
+        {selectedComponent === 1 && <StepOne />}
+        {selectedComponent === 2 && <StepTwo />}
+        {selectedComponent === 3 && <StepThree />}
+        {selectedComponent === 4 && <StepFour />}
+        {selectedComponent === 5 && <StepFive />}
+        {selectedComponent === 6 && <StepSix />}
+        <CardActions>
+          {selectedComponent === 1 ? null : (
+            <Button type="button" variant="contained" onClick={() => setSelectedComponent(selectedComponent - 1)}>
+              이전 단계
+            </Button>
+          )}
+          {selectedComponent === 6 ? (
+            <Button type="submit" variant="contained">
+              제출
+            </Button>
+          ) : (
+            <Button type="button" variant="contained" onClick={() => setSelectedComponent(selectedComponent + 1)}>
+              다음 단계
+            </Button>
+          )}
+        </CardActions>
       </Card>
     </div>
   );
