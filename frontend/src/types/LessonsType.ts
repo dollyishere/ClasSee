07321d12af62510c13ec9d9ae6d346d22040cbf1
김진호 <wlsghsla94@naver.com>
2023-02-01 01:02:42 +0900

    Feat: '[FE] MyCreatedLessonsMainpage의 요청 데이터 type 지정'
    types 폴더에 interface 이용하여 MyCreatedLessonsMainpage 요청 데이터 타입 지정
    
    [#59]

---
+++ frontend/src/types/LessonsType.ts
@@ -0,0 +1,11 @@
+export interface MyCreatedLessonsMainpageResponse {
+  id: number;
+  lessonImage: string;
+  teacher: string;
+  teacherImage: string;
+  name: string;
+  runningTime: string;
+  category: string;
+  rating: number;
+  isBookMarked: boolean;
+}
