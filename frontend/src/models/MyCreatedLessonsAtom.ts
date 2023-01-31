c4681598109bc57d20a844ac41c576981997b423
김진호 <wlsghsla94@naver.com>
2023-02-01 01:00:08 +0900

    docs: '[FE] atom 파일 이름 수정'
    메인페이지의 내가 개설한 강의와 마이페이지
    의 내가 개설한 강의가 겹칠것을 대비하여 이름 수정
    
    [#59]

--- frontend/src/models/MyCreatedLessonsAtom.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { atom } from 'recoil';
-
-const MyCreatedLessonsState = atom({
-  key: 'MyCreatedLessonsData',
-  default: [],
-});
-
-export default MyCreatedLessonsState;
