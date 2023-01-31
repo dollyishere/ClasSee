8f2b074c3abca3eca6cd676bf2cbb12e40bc3299
김진호 <wlsghsla94@naver.com>
2023-02-01 01:06:43 +0900

    Feat: '[FE] getMyCreatedLessonsMainpage viewmodel 개발'
    메인페이지의 내가 개설한강의 2개를 받아오는 api를 요청하는 viewmodel을 개발
    
    [#59]

--- frontend/src/viewmodels/MainPageViewModel.ts
+++ frontend/src/viewmodels/MainPageViewModel.ts
@@ -1,78 +1,17 @@
-import MyCreatedLessonsApi from '../apis/LessonsApi';
+import axios from 'axios';
+import useApi from '../apis/LessonsApi';
+import { MyCreatedLessonsMainpageResponse } from '../types/LessonsType';
 
 const MainPageViewModel = () => {
-  const MyCreatedLessons = async () => {
-    const response = await <MyCreatedLessonsApi />;
-    return response.data;
-  };
+  const { MyCreatedLessonsMainpageApi } = useApi();
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  const getMyCreatedLessonsMainpage = async (userId: number) => {
+    const res = await MyCreatedLessonsMainpageApi(userId);
 
-  const createToken = async (sessionId: string) => {
-    const response = await axios.post(
-      `http://localhost:5000/api/sessions/${sessionId}/connections`,
-      {},
-      {
-        headers: { 'Content-Type': 'application/json' },
-      },
-    );
-
-    return response.data;
-  };
-
-  const createSession = async (sessionId: string) => {
-    const response = await axios.post(
-      `http://localhost:5000/api/sessions`,
-      { customSessionId: sessionId },
-      { headers: { 'Content-Type': 'application/json' } },
-    );
-    return response.data;
-  };
-  const getToken = async (mySessionId: string) => {
-    const sessionId = await createSession(mySessionId);
-    const token = await createToken(sessionId);
-    return token;
-  };
-
-  const chat = (session: Session, message: string) => {
-    session
-      .signal({
-        data: message,
-        to: [],
-        type: 'chat',
-      })
-      .then(() => {
-        console.log('Message successfully sent');
-      })
-      .catch((error: any) => {
-        console.log(error);
-      });
+    return res;
   };
   return {
-    getToken,
-    chat,
+    getMyCreatedLessonsMainpage,
   };
 };
 
