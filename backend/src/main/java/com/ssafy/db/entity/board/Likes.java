091ec79dc67bf70a303714514eb88f2500600b96
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-01 11:58:06 +0900

    [Feat]: [BE] 좋아요 테이블 어노테이션 추가

--- backend/src/main/java/com/ssafy/db/entity/board/Likes.java
+++ backend/src/main/java/com/ssafy/db/entity/board/Likes.java
@@ -17,7 +17,7 @@ import javax.persistence.*;
 public class Likes {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
