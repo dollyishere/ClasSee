72a59274d4c72f0c61254d7a9f769f054fc07a1c
No88888888 <isagkim@gmail.com>
2023-02-01 13:26:10 +0900

    Feat: '[FE] LessonsApi 작성' userId의 typw 수정, return response를 response.data로 수정, import받는 lessonsresponse 이름 수정
    [#53]

--- frontend/src/apis/LessonsApi.ts
+++ frontend/src/apis/LessonsApi.ts
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import { MyCreatedLessonsMainpageResponse } from '../types/LessonsType';
+import axios, { AxiosResponse } from 'axios';
+import { LessonsResponse } from '../types/LessonsType';
 
 const LessonsApi = () => {
   // 내가 개설한 강의 2개 불러오는 함수
-  const MyCreatedLessonsMainpageApi = async (userId: number) => {
+  const MyCreatedLessonsMainpageApi = async (userId: string) => {
     try {
       const response = await axios.get(
         `${process.env.REACT_APP_SERVER_URI}/api/v1/mycreatedlessonsmainpage/${userId}`,
       );
-      return response;
+      return response.data;
     } catch (error: any) {
       console.error(error);
     }
