88fde5e57e6a8fa679656adee70a10caec99f699
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-01 16:45:45 +0900

    [Feat]: [BE] 포토카드 좋아요를 위한 일부 어노테이션 추가

--- backend/src/main/java/com/ssafy/db/entity/board/Photocard.java
+++ backend/src/main/java/com/ssafy/db/entity/board/Photocard.java
@@ -10,6 +10,8 @@ import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 * member : photocard = 1 : N
@@ -41,4 +43,7 @@ public class Photocard {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "lesson_id")
     private Lesson lesson;
+
+    @OneToMany(mappedBy = "photocard", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Likes> likesList = new ArrayList<>();
 }
