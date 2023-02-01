76ceccc15d40184ced02ee22b732c98525da4ee3
No88888888 <isagkim@gmail.com>
2023-02-01 17:49:23 +0900

    Refactor: '[FE] 카드 컴포넌트 refactoring'
    카드 컴포넌틑를 감싸는 div를 기존 각 강의 컴포넌트에서 lessoncard 컴포넌트 안으로 삽입
    
    [#235]

--- frontend/src/components/LessonCard.tsx
+++ frontend/src/components/LessonCard.tsx
@@ -32,70 +32,76 @@ const LessonCard = ({ lesson }: Props) => {
   };
 
   return (
-    <Link to={`/lessons/${lesson.id}`} className="lesson__card" key={lesson.id}>
-      {/* 강의 대표이미지와 북마크 버튼 담는 div */}
-      <div className="lesson__backImg">
-        <img className="lesson__img" src={logo} alt={lesson.name} />
-        {/* 북마크 버튼 클릭 시 true, false 값변경으로 아이콘 변경 */}
-        <button
-          type="button"
-          onClick={getBookmarkStatus}
-          className="lesson__bookmark"
-        >
-          {isBookMarked ? (
-            <BookmarkIcon
-              fontSize="large"
-              color="error"
-              className="lesson__bookmark--icon"
-            />
-          ) : (
-            <BookmarkBorderIcon
-              fontSize="large"
-              color="action"
-              className="lesson__bookmark--icon"
-            />
-          )}
-        </button>
-      </div>
-      {/* 카테고리, 강사 이미지 */}
-      <div className="lesson__categoryteacherImage">
-        <div className="lesson__category">
-          <p className="lesson__category--text">{lesson.category}</p>
+    <div className="lesson">
+      <Link
+        to={`/lessons/${lesson.id}`}
+        className="lesson__card"
+        key={lesson.id}
+      >
+        {/* 강의 대표이미지와 북마크 버튼 담는 div */}
+        <div className="lesson__backImg">
+          <img className="lesson__img" src={logo} alt={lesson.name} />
+          {/* 북마크 버튼 클릭 시 true, false 값변경으로 아이콘 변경 */}
+          <button
+            type="button"
+            onClick={getBookmarkStatus}
+            className="lesson__bookmark"
+          >
+            {isBookMarked ? (
+              <BookmarkIcon
+                fontSize="large"
+                color="error"
+                className="lesson__bookmark--icon"
+              />
+            ) : (
+              <BookmarkBorderIcon
+                fontSize="large"
+                color="action"
+                className="lesson__bookmark--icon"
+              />
+            )}
+          </button>
         </div>
-        <Stack
-          className="lesson__teacherImage--image"
-          direction="row"
-          spacing={2}
-        >
-          <Avatar alt="Remy Sharp" src={logo} />
-        </Stack>
-      </div>
-      {/* 강의명 */}
-      <p className="lesson__name">{lesson.name}</p>
-      {/* 강의 소요시간 */}
-      <div className="lesson__ratingtime">
-        {/* 강의 평점 */}
-        <div className="lesson__rating">
-          {/* 별 아이콘 0.5점 단위 */}
-          <Rating
-            className="lesson__rating--star"
-            name="half-rating-read"
-            value={lesson.rating}
-            precision={0.5}
-            readOnly
-          />
-          {/* 별점 숫자 */}
-          <p className="lesson__rating--number"> {lesson.rating}</p>
+        {/* 카테고리, 강사 이미지 */}
+        <div className="lesson__categoryteacherImage">
+          <div className="lesson__category">
+            <p className="lesson__category--text">{lesson.category}</p>
+          </div>
+          <Stack
+            className="lesson__teacherImage--image"
+            direction="row"
+            spacing={2}
+          >
+            <Avatar alt="Remy Sharp" src={logo} />
+          </Stack>
         </div>
-        {/* 소요시간 */}
-        <p className="lesson__time">
-          {/* 소요시간 아이콘 */}
-          <AvTimerIcon />
-          {/* 소요시간 숫자 */}
-          {lesson.runningTime} 시간 소요
-        </p>
-      </div>
-    </Link>
+        {/* 강의명 */}
+        <p className="lesson__name">{lesson.name}</p>
+        {/* 강의 소요시간 */}
+        <div className="lesson__ratingtime">
+          {/* 강의 평점 */}
+          <div className="lesson__rating">
+            {/* 별 아이콘 0.5점 단위 */}
+            <Rating
+              className="lesson__rating--star"
+              name="half-rating-read"
+              value={lesson.rating}
+              precision={0.5}
+              readOnly
+            />
+            {/* 별점 숫자 */}
+            <p className="lesson__rating--number"> {lesson.rating}</p>
+          </div>
+          {/* 소요시간 */}
+          <p className="lesson__time">
+            {/* 소요시간 아이콘 */}
+            <AvTimerIcon />
+            {/* 소요시간 숫자 */}
+            {lesson.runningTime} 시간 소요
+          </p>
+        </div>
+      </Link>
+    </div>
   );
 };
 
