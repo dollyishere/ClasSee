b608b9eb2a529da2b7aa7089f7c2eebfd36880db
김진호 <wlsghsla94@naver.com>
2023-02-02 22:18:35 +0900

    Feat: '[FE] 메인페이지 내가 신청한 강의용 카드 컴포넌트 개발'
    기존 카드 컴포넌트에 hover기능과
    스케쥴이 추가된 myappliedlessonscard 컴포넌트 개발
    
    [#218]

---
+++ frontend/src/components/MyAppliedLessonCardTest.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { atom, selector, useRecoilState } from 'recoil';
+import { Link } from 'react-router-dom';
+import Rating from '@mui/material/Rating';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import AvTimerIcon from '@mui/icons-material/AvTimer';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import { LessonsResponse, Lesson, MyAppliedHover } from '../types/LessonsType';
+import logo from '../assets/logo.png';
+
+// type combinetype = { lesson: Lesson; myapplied: MyAppliedHover };
+interface Props {
+  lesson: Lesson;
+}
+const MyAppliedTest = ({ lesson }: Props) => {
+  const [isBookMarked, setIsBookMarked] = useState(lesson.isBookMarked);
+  // 북마크 아이콘 클릭 시 북마크 추가, 삭제 토글 버튼 함수
+  const getBookmarkStatus = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setIsBookMarked(!isBookMarked);
+  };
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div
+      className="lesson__card"
+      onMouseEnter={() => {
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+    >
+      {/* 강의 대표이미지와 북마크 버튼 담는 div */}
+      <div className="lesson__backImg">
+        <img className="lesson__img" src={logo} alt={lesson.name} />
+        {/* 북마크 버튼 클릭 시 true, false 값변경으로 아이콘 변경 */}
+        <button
+          type="button"
+          onClick={getBookmarkStatus}
+          className="lesson__bookmark"
+        >
+          {isBookMarked ? (
+            <BookmarkIcon
+              fontSize="large"
+              color="error"
+              className="lesson__bookmark--icon"
+            />
+          ) : (
+            <BookmarkBorderIcon
+              fontSize="large"
+              color="action"
+              className="lesson__bookmark--icon"
+            />
+          )}
+        </button>
+      </div>
+      {/* 카테고리, 강사 이미지 */}
+      <div className="lesson__categoryteacherImage">
+        <div className="lesson__category">
+          <p className="lesson__category--text">{lesson.category}</p>
+        </div>
+        <Stack
+          className="lesson__teacherImage--image"
+          direction="row"
+          spacing={2}
+        >
+          <Avatar alt="Remy Sharp" src={logo} />
+        </Stack>
+      </div>
+      {/* 강의명 */}
+      <p className="lesson__name">{lesson.name}</p>
+      {/* 강의 소요시간 */}
+      <div className="lesson__ratingtime">
+        {/* 강의 평점 */}
+        <div className="lesson__rating">
+          {/* 별 아이콘 0.5점 단위 */}
+          <Rating
+            className="lesson__rating--star"
+            name="half-rating-read"
+            value={lesson.rating}
+            precision={0.5}
+            readOnly
+          />
+          {/* 별점 숫자 */}
+          <p className="lesson__rating--number"> {lesson.rating}</p>
+        </div>
+        {/* 소요시간 */}
+        <p className="lesson__time">
+          {/* 소요시간 아이콘 */}
+          <AvTimerIcon />
+          {/* 소요시간 숫자 */}
+          {lesson.runningTime} 시간 소요
+        </p>
+      </div>
+      <div className="applylessons__schedule">
+        <h3>클래스 시작</h3>
+        <p> {lesson.startTime}</p>
+      </div>
+      {isHovered && (
+        <div className="applylessons__hover">
+          {/* <Link to={`/participant/${lesson.id}`}> */}
+          <div className="applylessons__hover--buttons">
+            <div>
+              <Link to="/lesson/test/student">
+                <button className="applylessons__hover--partici" type="button">
+                  Participant
+                </button>
+              </Link>
+            </div>
+            <div>
+              <button className="lesson__card-delete-button" type="button">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyAppliedTest;
