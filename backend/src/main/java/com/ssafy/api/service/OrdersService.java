6a3e27ffbcd8b49159e8bc8892a363389b010c9e
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-02 09:54:24 +0900

    [Feat]: [BE] 주문 서비스 생성

---
+++ backend/src/main/java/com/ssafy/api/service/OrdersService.java
@@ -0,0 +1,4 @@
+package com.ssafy.api.service;
+
+public interface OrdersService {
+}
---
+++ backend/src/main/java/com/ssafy/api/service/OrdersServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ssafy.api.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class OrdersServiceImpl implements OrdersService{
+}
