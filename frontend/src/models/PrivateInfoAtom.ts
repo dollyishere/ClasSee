d5e23c879286c422d9ee3d4ed9f555c8f9720bc0
김진호 <wlsghsla94@naver.com>
2023-02-03 00:41:58 +0900

    Refactor: '[FE] atom에 저장될 userId의 기본값을 임의로 지정'
    type 에러 방지
    
    [#66]

--- frontend/src/models/PrivateInfoAtom.ts
+++ frontend/src/models/PrivateInfoAtom.ts
@@ -5,7 +5,7 @@ import { UserInfo } from '../types/UserType';
 const PrivateInfoState = atom<UserInfo>({
   key: 'PrivateInfoData',
   default: {
-    userId: null,
+    userId: 'ssafy1234@ssafy.ac.kr',
   },
 });
 export default PrivateInfoState;
