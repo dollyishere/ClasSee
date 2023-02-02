00478040a62c6b900a67761d1fbe6d78cff000b4
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-02 13:02:28 +0900

    [Feat]: [BE] 주문 생성 로직 컨트롤러 추가

--- backend/src/main/java/com/ssafy/api/controller/OrdersController.java
+++ backend/src/main/java/com/ssafy/api/controller/OrdersController.java
@@ -1,8 +1,11 @@
 package com.ssafy.api.controller;
 
+import com.ssafy.api.request.OrdersRegistPostReq;
+import com.ssafy.api.request.PhotocardRegistPostReq;
 import com.ssafy.api.response.NoticeInfoRes;
 import com.ssafy.api.response.OrdersInfoGetRes;
 import com.ssafy.api.service.OrdersService;
+import com.ssafy.common.model.response.BaseResponseBody;
 import com.ssafy.db.entity.board.Notice;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -10,10 +13,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(value = "주문 API", tags = {"Orders"})
 @RestController
@@ -22,6 +22,19 @@ public class OrdersController {
 
     @Autowired
     OrdersService ordersService;
+
+    @PostMapping()
+    @ApiOperation(value = "주문 정보 등록", notes = "주문 정보 입력 후 주문 생성")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "success")
+    })
+    public ResponseEntity<? extends BaseResponseBody> registOrders(@RequestBody OrdersRegistPostReq ordersRegistPostReq) {
+
+        ordersService.createOrders(ordersRegistPostReq);
+
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200,"success"));
+
+    }
 
     @GetMapping()
     @ApiOperation(value = "주문페이지 정보", notes = "주문 페이지에 들어왔을 때, 필요한 정보들을 반환")
