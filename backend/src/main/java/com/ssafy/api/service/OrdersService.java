3d2526e3d56265a251b8be7849ee898eb5ae7e0a
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-02 12:25:00 +0900

    [Feat]: [BE] 주문화면 정보 관련 서비스 추가

--- backend/src/main/java/com/ssafy/api/service/OrdersService.java
+++ backend/src/main/java/com/ssafy/api/service/OrdersService.java
@@ -1,4 +1,8 @@
 package com.ssafy.api.service;
 
+import com.ssafy.api.response.OrdersInfoGetRes;
+
 public interface OrdersService {
+
+    OrdersInfoGetRes readOrders(String email, Long id);
 }
--- backend/src/main/java/com/ssafy/api/service/OrdersServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/OrdersServiceImpl.java
@@ -1,9 +1,43 @@
 package com.ssafy.api.service;
 
+import com.ssafy.api.response.OrdersInfoGetRes;
+import com.ssafy.db.entity.lesson.Lesson;
+import com.ssafy.db.entity.lesson.OpenLesson;
+import com.ssafy.db.entity.user.User;
+import com.ssafy.db.repository.LessonRepositorySupport;
+import com.ssafy.db.repository.OrdersRepositorySupport;
+import com.ssafy.db.repository.UserRepositorySupport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class OrdersServiceImpl implements OrdersService{
+
+    @Autowired
+    UserRepositorySupport userRepositorySupport;
+
+    @Autowired
+    OrdersRepositorySupport ordersRepositorySupport;
+
+    @Autowired
+    LessonRepositorySupport lessonRepositorySupport;
+
+    @Override
+    public OrdersInfoGetRes readOrders(String email, Long id) {
+
+        OpenLesson openLesson = ordersRepositorySupport.findOneOpenLesson(id);
+        Lesson lesson = ordersRepositorySupport.findOneLesson(openLesson.getLessonId());
+
+        Long user_id = userRepositorySupport.findId(email);
+        User user = userRepositorySupport.findOne(user_id);
+
+        String lesson_img = lessonRepositorySupport.findLessonProfileImg(lesson);
+
+        OrdersInfoGetRes ordersInfoGetRes = new OrdersInfoGetRes(openLesson, lesson, user);
+        ordersInfoGetRes.setLesson_img(lesson_img);
+
+        return ordersInfoGetRes;
+    }
 }
