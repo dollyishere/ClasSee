b3661cd579e628ffd619f5ca132ea6f22a5a7900
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-03 16:13:54 +0900

    [Feat]: [BE] Review관련 기본 클래스 생성

---
+++ backend/src/main/java/com/ssafy/api/service/ReviewServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ssafy.api.service;
+
+public class ReviewServiceImpl implements ReviewService {
+}
---
+++ backend/src/main/java/com/ssafy/db/repository/ReviewRepositorySupport.java
@@ -0,0 +1,17 @@
+package com.ssafy.db.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewRepositorySupport {
+
+    private final EntityManager em;
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+}
