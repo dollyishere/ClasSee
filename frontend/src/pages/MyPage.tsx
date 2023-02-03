befb1711c451054b1d5bcadf9a1c85007eebbd2d
고태진 <thereisnotruth12@gmail.com>
2023-02-03 15:46:42 +0900

    feat: '[FE] 회원정보 관련 페이지 한꺼번에 관리'
    내정보, 개설한 강의, 신청한 강의, 북마크, 포토북, 작성한 리뷰 MyPage 파일에서 라우팅
    
    [#166]

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -3,12 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import ProfilePage from './ProfilePage';
 import TestPage from './TestPage';
 
+import Header from '../components/Header';
+import Sidebar from '../components/mypage/Sidebar';
+
 const MyPage = () => {
   return (
-    <Routes>
-      <Route path="" element={<ProfilePage />} />
-      <Route path="/test" element={<TestPage />} />
-    </Routes>
+    <div className="my-page page">
+      <Header />
+      <div className="my-page__contents">
+        <Sidebar />
+        <Routes>
+          <Route path="" element={<ProfilePage />} />
+          <Route path="/test" element={<TestPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
