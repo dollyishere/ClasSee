042236397302f5b292557541c76b02fbd7b0f401
7unho <rlawnsgh8395@naver.com>
2023-02-03 13:06:43 +0900

    Fix: [BE] 로그인 응답 데이터 수정 bookmark 리스트 추가, point 수정

--- backend/src/main/java/com/ssafy/api/service/BookmarkService.java
+++ backend/src/main/java/com/ssafy/api/service/BookmarkService.java
@@ -2,10 +2,14 @@ package com.ssafy.api.service;
 
 import com.ssafy.api.request.BookmarkRegisterReq;
 
+import java.util.List;
+
 /**
  *	북마크 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
  */
 public interface BookmarkService {
     void create(BookmarkRegisterReq requestInfo);
     void delete(String email, Long lessonId);
+
+    List<Long> getBookmarkList(Long userId);
 }
