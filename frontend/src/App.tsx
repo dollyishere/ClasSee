e47829b17ea54a024a5071420aeca79a7fa356f6
고태진 <thereisnotruth12@gmail.com>
2023-02-06 05:25:39 +0900

    feat: '[FE] 강의 목록 틀 생성'
    강의 목록 페이지 url과 틀만 잡아놓았습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import MyPage from './pages/MyPage';
 import TestPage from './pages/TestPage';
 
 import Footer from './components/Footer';
+import LessonsPage from './pages/LessonsPage';
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/image" element={<TestPage />} />
           <Route path="/mypage/*" element={<MyPage />} />
+          <Route path="/lessons/*" element={<LessonsPage />} />
         </Routes>
       </div>
     </RecoilRoot>
