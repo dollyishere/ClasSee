adba8da4135cb49aaaa3e1679997e3a58d5b418b
7unho <rlawnsgh8395@naver.com>
2023-02-05 13:59:37 +0900

    Fix: [BE] 북마크 api url 수정;

--- backend/src/main/java/com/ssafy/api/service/BookmarkService.java
+++ backend/src/main/java/com/ssafy/api/service/BookmarkService.java
@@ -9,7 +9,7 @@ import java.util.List;
  *	북마크 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
  */
 public interface BookmarkService {
-    void create(BookmarkRegisterReq requestInfo);
+    void create(String email, Long lessonId);
     void delete(String email, Long lessonId);
 
     List<Lesson> getBookmarkList(Long userId);
