50aa99daa56984a8eb64be06121f23954b1feed2
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-05 17:41:23 +0900

    [Feat]: [BE] Review 작성 기능 리포지토리

--- backend/src/main/java/com/ssafy/db/repository/ReviewRepositorySupport.java
+++ backend/src/main/java/com/ssafy/db/repository/ReviewRepositorySupport.java
@@ -1,6 +1,9 @@
 package com.ssafy.db.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.db.entity.lesson.QLesson;
+import com.ssafy.db.entity.lesson.QReview;
+import com.ssafy.db.entity.lesson.Review;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +16,15 @@ public class ReviewRepositorySupport {
     private final EntityManager em;
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    QReview qReview = QReview.review;
+
+    QLesson qLesson = QLesson.lesson;
+
+    public void save(Review review) { em.persist(review); }
+
+    public void delete(Review review) { em.remove(review); }
+
+
 
 }
