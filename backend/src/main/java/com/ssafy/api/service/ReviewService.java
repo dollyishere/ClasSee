503e228a823bdbe4fea9acbc16967b125a6b6286
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-05 19:38:50 +0900

    [Feat]: [BE] Review 서비스 생성 관련 기능 추가

--- backend/src/main/java/com/ssafy/api/service/ReviewService.java
+++ backend/src/main/java/com/ssafy/api/service/ReviewService.java
@@ -1,4 +1,15 @@
 package com.ssafy.api.service;
 
+import com.ssafy.api.request.ReviewRegistPostReq;
+
 public interface ReviewService {
+
+    void createReview(ReviewRegistPostReq reviewRegistPostReq);
+
+    void readReview();
+
+    void updateReview();
+
+    void deleteReview();
+
 }
--- backend/src/main/java/com/ssafy/api/service/ReviewServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/ReviewServiceImpl.java
@@ -1,5 +1,14 @@
 package com.ssafy.api.service;
 
+import com.ssafy.api.request.ReviewRegistPostReq;
+import com.ssafy.db.entity.lesson.Lesson;
+import com.ssafy.db.entity.lesson.OpenLesson;
+import com.ssafy.db.entity.lesson.Review;
+import com.ssafy.db.entity.user.User;
+import com.ssafy.db.repository.OrdersRepositorySupport;
+import com.ssafy.db.repository.ReviewRepositorySupport;
+import com.ssafy.db.repository.UserRepositorySupport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,6 +16,57 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ReviewServiceImpl implements ReviewService {
 
+    @Autowired
+    ReviewRepositorySupport reviewRepositorySupport;
 
+    @Autowired
+    UserRepositorySupport userRepositorySupport;
 
+    @Autowired
+    OrdersRepositorySupport ordersRepositorySupport;
+
+    @Override
+    public void createReview(ReviewRegistPostReq reviewRegistPostReq) {
+
+        Long user_id = userRepositorySupport
+                .findId(reviewRegistPostReq.getUser_email());
+
+        User user = userRepositorySupport
+                .findOne(user_id);
+
+        OpenLesson openLesson = ordersRepositorySupport
+                .findOneOpenLesson(reviewRegistPostReq.getOpenLesson_id());
+
+        Lesson lesson = ordersRepositorySupport
+                .findOneLesson(openLesson.getLessonId());
+
+        Review review = Review.builder()
+                .title(reviewRegistPostReq.getTitle())
+                .content(reviewRegistPostReq.getContent())
+                .img(reviewRegistPostReq.getImg())
+                .score(reviewRegistPostReq.getScore())
+                .user(user)
+                .lesson(lesson)
+                .build();
+
+        reviewRepositorySupport.save(review);
+
+        return;
+
+    }
+
+    @Override
+    public void readReview() {
+
+    }
+
+    @Override
+    public void updateReview() {
+
+    }
+
+    @Override
+    public void deleteReview() {
+
+    }
 }
