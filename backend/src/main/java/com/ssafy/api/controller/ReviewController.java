72d35c22a524eed8c89fcb5476644dc2dfb40140
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-05 17:41:39 +0900

    [Feat]: [BE] Review 작성 기능 컨트롤러

--- backend/src/main/java/com/ssafy/api/controller/ReviewController.java
+++ backend/src/main/java/com/ssafy/api/controller/ReviewController.java
@@ -1,4 +1,11 @@
 package com.ssafy.api.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "Review API", tags = {"Review"})
+@RestController
+@RequestMapping("/api/v1/review")
 public class ReviewController {
 }
