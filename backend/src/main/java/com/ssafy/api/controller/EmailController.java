a5f7fe0fd292df42f3d7c1b90d3648e3f5d34425
7unho <rlawnsgh8395@naver.com>
2023-02-05 14:01:05 +0900

    Fix: [BE] mail api url 수정

--- backend/src/main/java/com/ssafy/api/controller/EmailController.java
+++ backend/src/main/java/com/ssafy/api/controller/EmailController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Api(value = "메일", tags = {"Mail"})
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/mails")
 @RestController
 public class EmailController {
 
@@ -17,9 +17,9 @@ public class EmailController {
         this.emailService = emailService;
     }
 
-    @GetMapping("/mailConfirm")
+    @GetMapping("/confirm/{email}")
     @ResponseBody
-    public String mailConfirm(@RequestParam String email) throws Exception {
+    public String mailConfirm(@PathVariable String email) throws Exception {
         String code = emailService.sendSimpleMessage(email);
         System.out.println("인증코드: " + code);
         return code;
