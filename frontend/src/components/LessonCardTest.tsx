9ff77002591c26194816d9ed770afd1ccbdddb66
김진호 <wlsghsla94@naver.com>
2023-02-05 16:20:37 +0900

    Feat: '[FE] LessonCard에 북마크 함수 구현'
    로그인 상태와 디폴트 북마크 정보를 기반으로 기 북마크 시 북마크 삭제 api
    미 북마크 시 북마크 추가 api를 요청
    
    [#269]

--- frontend/src/components/LessonCardTest.tsx
+++ frontend/src/components/LessonCardTest.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { atom, selector, useRecoilState } from 'recoil';
+import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 import { Link } from 'react-router-dom';
 import Rating from '@mui/material/Rating';
 import Avatar from '@mui/material/Avatar';
@@ -7,7 +7,10 @@ import Stack from '@mui/material/Stack';
 import AvTimerIcon from '@mui/icons-material/AvTimer';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import { LessonsResponse, Lesson, MyAppliedHover } from '../types/LessonsType';
+import { LessonsResponse, Lesson } from '../types/LessonsType';
+import PrivateInfoState from '../models/PrivateInfoAtom';
+import useViewModel from '../viewmodels/MainPageViewModel';
+
 import logo from '../assets/logo.png';
 
 // type combinetype = { lesson: Lesson; myapplied: MyAppliedHover };
@@ -16,10 +19,21 @@ interface Props {
 }
 const LessonCard = ({ lesson }: Props) => {
   const [isBookMarked, setIsBookMarked] = useState(lesson.isBookMarked);
+  const userInfo = useRecoilValue(PrivateInfoState);
+
+  const { deleteBookmark, addBookmark } = useViewModel();
   // 북마크 아이콘 클릭 시 북마크 추가, 삭제 토글 버튼 함수
   const getBookmarkStatus = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
-    setIsBookMarked(!isBookMarked);
+    if (userInfo) {
+      if (isBookMarked) {
+        deleteBookmark(userInfo.email, lesson.id);
+      } else {
+        addBookmark(userInfo.email, lesson.id);
+      }
+    } else {
+      window.confirm('로그인 후 사용 가능합니다');
+    }
   };
   return (
     <div className="lesson">
