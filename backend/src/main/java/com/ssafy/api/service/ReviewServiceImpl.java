06244663cb987a7e57e3375e71078906068b9a83
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-05 17:41:30 +0900

    [Feat]: [BE] Review 작성 기능 서비스

--- backend/src/main/java/com/ssafy/api/service/ReviewServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/ReviewServiceImpl.java
@@ -1,4 +1,12 @@
 package com.ssafy.api.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class ReviewServiceImpl implements ReviewService {
+
+
+
 }
