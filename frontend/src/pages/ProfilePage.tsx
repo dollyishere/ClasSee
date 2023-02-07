a609c73edf38a93468be80479b3b62599cfe37b1
고태진 <thereisnotruth12@gmail.com>
2023-02-07 15:54:31 +0900

    fix: '[FE] 닉네임 변경시 기본값으로 이름이 표시되는 버그 수정'
    userInfo.name -> userInfo.nickname

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -37,7 +37,7 @@ const ProfilePage = () => {
 
   const handleUpdateNickName = async () => {
     if (userInfo !== null) {
-      const nickname = prompt('변경할 닉네임을 입력하세요.', userInfo.name);
+      const nickname = prompt('변경할 닉네임을 입력하세요.', userInfo.nickname);
       if (nickname !== null) {
         updateNickName(nickname);
       }
