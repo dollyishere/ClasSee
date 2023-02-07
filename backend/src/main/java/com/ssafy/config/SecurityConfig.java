285c23f367acb62605184cc3300d0decbd3def52
7unho <rlawnsgh8395@naver.com>
2023-02-07 16:33:17 +0900

    Fix:[BE] SecurityConfig 수정

--- backend/src/main/java/com/ssafy/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -80,6 +80,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService, redisTemplate)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
                     .authorizeRequests()
+                    .antMatchers("/api/v1/auth/login").permitAll()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
+                    .antMatchers("/api/v1/auth/kakao").permitAll()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
                     .antMatchers(HttpMethod.GET, OPEN_API_PATH).permitAll()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
 //                    .antMatchers("/api/v1/users/me", "/api/v1/auth/test").authenticated()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
                     .anyRequest().authenticated()
