d22b3cbea1151ac877d1451a2684dbabc42d6d7d
Son Jeong Hun <thswjdgns9@naver.com>
2023-02-09 12:58:19 +0900

    [feat]: [BE] 포토카드 예외 카드 추가

--- backend/src/main/java/com/ssafy/api/controller/PhotocardController.java
+++ backend/src/main/java/com/ssafy/api/controller/PhotocardController.java
@@ -2,12 +2,18 @@ package com.ssafy.api.controller;
 
 import com.ssafy.api.request.ArticleRegisterPostReq;
 import com.ssafy.api.request.PhotocardRegistPostReq;
+import com.ssafy.api.response.LessonInfoListRes;
 import com.ssafy.api.response.PageGetRes;
 import com.ssafy.api.response.PhotocardListGetRes;
 import com.ssafy.api.response.PhotocardPageGetRes;
 import com.ssafy.api.service.PhotocardService;
+import com.ssafy.common.exception.handler.LessonException;
+import com.ssafy.common.exception.handler.PhotocardException;
+import com.ssafy.common.exception.handler.UserException;
 import com.ssafy.common.model.response.BaseResponseBody;
+import com.ssafy.common.model.response.InvalidErrorResponseBody;
 import com.ssafy.common.model.response.NotFoundErrorResponseBody;
+import com.ssafy.common.model.response.ServerErrorResponseBody;
 import com.ssafy.db.entity.board.Photocard;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -33,11 +39,22 @@ public class PhotocardController {
     @PostMapping()
     @ApiOperation(value = "포토카드 등록, 로그인 O", notes = "포토카드 정보 입력 후 포토카드 생성")
     @ApiResponses({
-            @ApiResponse(code = 200, message = "SUCCESS", response = BaseResponseBody.class)
+            @ApiResponse(code = 200, message = "성공", response = BaseResponseBody.class),
+            @ApiResponse(code = 401, message = "인증 실패", response = InvalidErrorResponseBody.class),
+            @ApiResponse(code = 404, message = "해당 자료 없음", response = NotFoundErrorResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = ServerErrorResponseBody.class)
     })
     public ResponseEntity<? extends BaseResponseBody> registPhotocard(@RequestBody PhotocardRegistPostReq photocardRegistPostReq) {
 
-        photocardService.createPhotocard(photocardRegistPostReq);
+        try {
+            photocardService.createPhotocard(photocardRegistPostReq);
+        } catch (UserException u){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404,"user not found"));
+        } catch (LessonException l){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404, "lesson not found"));
+        } catch (Exception e){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404, "unexpected exception"));
+        }
 
         return ResponseEntity.status(200).body(BaseResponseBody.of(200,"SUCCESS"));
 
@@ -46,11 +63,22 @@ public class PhotocardController {
     @PostMapping("/likes")
     @ApiOperation(value = "좋아요 등록, 로그인 O", notes = "좋아요를 누른 사용자 email, 포토카드 id를 입력 받아 좋아요 등록")
     @ApiResponses({
-            @ApiResponse(code = 200, message = "SUCCESS", response = BaseResponseBody.class)
+            @ApiResponse(code = 200, message = "성공", response = BaseResponseBody.class),
+            @ApiResponse(code = 401, message = "인증 실패", response = InvalidErrorResponseBody.class),
+            @ApiResponse(code = 404, message = "해당 자료 없음", response = NotFoundErrorResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = ServerErrorResponseBody.class)
     })
     public ResponseEntity<? extends BaseResponseBody> registLikes(@RequestParam String email, @RequestParam Long id) {
 
-        photocardService.createLikes(email, id);
+        try {
+            photocardService.createLikes(email, id);
+        } catch (UserException u){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404,"user not found"));
+        } catch (PhotocardException p){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404, "photocard not found"));
+        } catch (Exception e){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404,"unexpected exception"));
+        }
 
         return ResponseEntity.status(200).body(BaseResponseBody.of(200,"SUCCESS"));
 
@@ -61,7 +89,10 @@ public class PhotocardController {
     @ApiOperation(value = "포토카드 리스트, 로그인 X", notes = "limit는 가져올 갯수, offset은 시작 위치(0부터 시작), count는 총 개수," +
             " likes_count는 해당 포토카드의 좋아요 총개수, likes_check는 true면 내가 좋아요 누른 포토카드이고 false면 안누른 포토카드")
     @ApiResponses({
-            @ApiResponse(code = 200, message = "SUCCESS", response = PhotocardPageGetRes.class)
+            @ApiResponse(code = 200, message = "성공", response = PhotocardPageGetRes.class),
+            @ApiResponse(code = 401, message = "인증 실패", response = InvalidErrorResponseBody.class),
+            @ApiResponse(code = 404, message = "해당 자료 없음", response = NotFoundErrorResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = ServerErrorResponseBody.class)
     })
     public ResponseEntity<?> photocardList(@RequestParam int offset, @RequestParam int limit, String email){
 
@@ -103,13 +134,21 @@ public class PhotocardController {
     @ApiOperation(value = "나의 포토카드 리스트, 로그인 O", notes = "limit는 가져올 갯수, offset은 시작 위치(0부터 시작), count는 총 개수," +
             " likes_count는 해당 포토카드의 좋아요 총개수, likes_check는 true면 내가 좋아요 누른 포토카드이고 false면 안누른 포토카드")
     @ApiResponses({
-            @ApiResponse(code = 200, message = "SUCCESS", response = PhotocardPageGetRes.class)
+            @ApiResponse(code = 200, message = "성공", response = PhotocardPageGetRes.class),
+            @ApiResponse(code = 401, message = "인증 실패", response = InvalidErrorResponseBody.class),
+            @ApiResponse(code = 404, message = "해당 자료 없음", response = NotFoundErrorResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = ServerErrorResponseBody.class)
     })
     public ResponseEntity<?> photocardMyList(@RequestParam int offset, @RequestParam int limit, @PathVariable String email){
 
-        List<Photocard> photocardList =
-                photocardService
-                        .readMyPhotocardList(offset, limit, email);
+        List<Photocard> photocardList = new ArrayList<>();
+
+        try {
+            photocardList = photocardService
+                    .readMyPhotocardList(offset, limit, email);
+        } catch (UserException u){
+            return ResponseEntity.status(404).body("user not found");
+        }
 
         List<PhotocardListGetRes> photocardListGetResList =
                 photocardList
@@ -132,11 +171,18 @@ public class PhotocardController {
     @DeleteMapping()
     @ApiOperation(value = "포토 카드 삭제, 로그인 O", notes = "포토 카드 id를 입력 받아 삭제")
     @ApiResponses({
-            @ApiResponse(code = 200, message = "SUCCESS", response = BaseResponseBody.class)
+            @ApiResponse(code = 200, message = "성공", response = BaseResponseBody.class),
+            @ApiResponse(code = 401, message = "인증 실패", response = InvalidErrorResponseBody.class),
+            @ApiResponse(code = 404, message = "해당 자료 없음", response = NotFoundErrorResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = ServerErrorResponseBody.class)
     })
     public ResponseEntity<? extends BaseResponseBody> deletePhotocard(@RequestParam Long id) {
 
-        photocardService.deletePhotocard(id);
+        try {
+            photocardService.deletePhotocard(id);
+        } catch (PhotocardException p){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404,"photocard not found"));
+        }
 
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "SUCCESS"));
     }
@@ -144,15 +190,24 @@ public class PhotocardController {
     @DeleteMapping("/likes")
     @ApiOperation(value = "좋아요 취소, 로그인 O", notes = "좋아요를 취소할 사용자 email, 포토카드 id를 입력 받아 좋아요 취소(삭제)")
     @ApiResponses({
-            @ApiResponse(code = 200, message = "SUCCESS", response = BaseResponseBody.class)
+            @ApiResponse(code = 200, message = "성공", response = BaseResponseBody.class),
+            @ApiResponse(code = 401, message = "인증 실패", response = InvalidErrorResponseBody.class),
+            @ApiResponse(code = 404, message = "해당 자료 없음", response = NotFoundErrorResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = ServerErrorResponseBody.class)
     })
     public ResponseEntity<? extends BaseResponseBody> deleteLikes(@RequestParam String email, @RequestParam Long id) {
 
-        photocardService.deleteLikes(email, id);
+        try {
+            photocardService.deleteLikes(email, id);
+        } catch (UserException u){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404, "user not found"));
+        } catch (PhotocardException p){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404,"likes not found"));
+        } catch (Exception e){
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404,"unexpected exception"));
+        }
 
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "SUCCESS"));
     }
-
-
 
 }
--- backend/src/main/java/com/ssafy/api/service/PhotocardService.java
+++ backend/src/main/java/com/ssafy/api/service/PhotocardService.java
@@ -1,22 +1,24 @@
 package com.ssafy.api.service;
 
 import com.ssafy.api.request.PhotocardRegistPostReq;
+import com.ssafy.common.exception.handler.PhotocardException;
+import com.ssafy.common.exception.handler.UserException;
 import com.ssafy.db.entity.board.Photocard;
 
 import java.util.List;
 
 public interface PhotocardService {
 
-    void createPhotocard(PhotocardRegistPostReq photocardRegistPostReq);
+    void createPhotocard(PhotocardRegistPostReq photocardRegistPostReq) throws Exception;
     List<Photocard> readPhotocardList(int offset, int limit);
-    List<Photocard> readMyPhotocardList(int offset, int limit, String email);
+    List<Photocard> readMyPhotocardList(int offset, int limit, String email) throws UserException;
     Long photocardCount();
     Long myPhotocardCount(String email);
-    void deletePhotocard(Long id);
+    void deletePhotocard(Long id) throws PhotocardException;
     Long countLikes(Photocard photocard);
     Boolean checkLikes(String email, Long id);
-    void createLikes(String email, Long id);
-    void deleteLikes(String email, Long id);
+    void createLikes(String email, Long id) throws Exception;
+    void deleteLikes(String email, Long id) throws Exception;
 
 
 
