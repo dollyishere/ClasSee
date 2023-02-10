099e4ebfbbc846758b2129f712d2d6aa78ef6857
7unho <rlawnsgh8395@naver.com>
2023-02-10 15:47:54 +0900

    Fix: [BE] 북마크 등록 api 수정 등록 성공 시 200, 실패 시 401 코드 반환

--- backend/src/main/java/com/ssafy/api/service/BookmarkService.java
+++ backend/src/main/java/com/ssafy/api/service/BookmarkService.java
@@ -9,7 +9,7 @@ import java.util.List;
  *	북마크 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
  */
 public interface BookmarkService {
-    void create(Long userId, Long lessonId);
+    int create(Long userId, Long lessonId);
     void delete(String email, Long lessonId);
 
     List<Lesson> getBookmarkList(Long userId, int limit, int offset);
