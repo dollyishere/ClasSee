8a7b46b9ba9129298634c3e13343a2fb98f35f4a
고태진 <thereisnotruth12@gmail.com>
2023-02-12 17:50:24 +0900

    fix: '[FE] 필요없는 argument 제거'
    getMyCreatedLessonMainPage의 4번째 argmuent 제거

--- frontend/src/pages/MyCreatedLessonsPage.tsx
+++ frontend/src/pages/MyCreatedLessonsPage.tsx
@@ -39,7 +39,6 @@ const MyCreatedLessonsPage = () => {
           userInfo.email,
           limit,
           offset,
-          'DONE',
         );
         console.log(data.lessonInfoList);
         setLessons(data.lessonInfoList);
