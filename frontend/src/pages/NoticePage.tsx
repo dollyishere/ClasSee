50f6ccb715b8c0f95e3046a123da03851a879b24
고태진 <thereisnotruth12@gmail.com>
2023-02-13 10:39:33 +0900

    feat: '[FE] 공지사항 작성 페이지 링크 등록'
    사용자가 관리자면 공지사항 작성 페이지로 이동

--- frontend/src/pages/NoticePage.tsx
+++ frontend/src/pages/NoticePage.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+
 import Header from '../components/Header';
+import PrivateInfoState from '../models/PrivateInfoAtom';
 
 const NoticePage = () => {
   const [page, setPage] = useState<number>(0);
   const [notices, setNotices] = useState(null);
+  const userInfo = useRecoilValue(PrivateInfoState);
 
   useEffect(() => {
-    console.log('마운트');
+    console.log(userInfo);
   }, []);
   return (
     <div className="notice-page">
@@ -14,6 +19,11 @@ const NoticePage = () => {
       <div className="notice-page__contents">
         <div className="notice-page__title">공지사항</div>
         <div className="notice-page__list">
+          {userInfo && userInfo.userRole === 'ROLE_ADMIN' ? (
+            <div className="notice-page__write">
+              <Link to="write">공지사항 작성하기</Link>
+            </div>
+          ) : null}
           <ul>
             <li>공지</li>
           </ul>
