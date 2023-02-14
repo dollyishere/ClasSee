2c7ccbcfbcf965e1d1c5e1473b1acd98b2aa7a95
김진호 <wlsghsla94@naver.com>
2023-02-14 23:09:51 +0900

    Feat: '[FE] 내가 작성한 후기 페이지 구현'
    MyreviewsPage에서 내가 작성한 후기 불러오기
    ReviewItem에 prop보낸후 렌더링
    
    [#396]

--- frontend/src/pages/MyReviewsPage.tsx
+++ frontend/src/pages/MyReviewsPage.tsx
@@ -1,47 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { Pagination, Rating } from '@mui/material';
 import { useRecoilValue } from 'recoil';
-import { Pagination } from '@mui/material';
-import MyReviewsItem from '../components/MyReviewsItem';
 import PrivateInfoState from '../models/PrivateInfoAtom';
+import { ReviewType } from '../types/ReviewType';
+import ReviewItem from '../components/LessonDetailPage/ReviewItem';
+import useViewModel from '../viewmodels/ReviewViewModel';
 import useMyReviewViewModel from '../viewmodels/MyReviewsViewModel';
 
-interface MyReview {
-  id: number;
-  content: string;
-  img: string;
-  regtime: string;
-  lessonId: number;
-  lessonName: string;
-  score: number;
-  userEmail: string;
-  userImg: number | null;
-  userNickname: string;
-}
-const MyReviewsPage = () => {
-  const [page, setPage] = useState(1);
-  const [count, setCount] = useState<number>(0);
-  const [reviewList, setReviewList] = useState<MyReview[]>([]);
-  const [review, setReview] = useState<MyReview>({
-    id: 0,
-    content: '',
-    img: '',
-    regtime: '',
-    lessonId: 0,
-    lessonName: '',
-    score: 0,
-    userEmail: '',
-    userImg: 0,
-    userNickname: '',
-  });
-  const userInfo = useRecoilValue(PrivateInfoState);
+const MyReviewsPage: React.FC = () => {
   const { getMyReviews } = useMyReviewViewModel();
+
+  const userInfo = useRecoilValue(PrivateInfoState);
+  const [page, setPage] = useState<number>(1);
+  const [count, setCount] = useState<number>(1);
+  const [reviews, setReviews] = useState<Array<ReviewType>>([]);
+  const { deleteReview } = useViewModel();
+
   const handlePageChange = (
     event: React.ChangeEvent<unknown>,
     value: number,
   ) => {
     setPage(value);
   };
-
   const handleMyReviewData = async () => {
     const limit = 4;
     const offset = (page - 1) * limit;
@@ -50,22 +30,35 @@ const MyReviewsPage = () => {
       if (MyReviewData !== null && MyReviewData.count !== undefined) {
         setCount(Math.ceil(MyReviewData.count / limit));
       }
-      setReviewList(MyReviewData?.page);
+      setReviews(MyReviewData?.page);
       console.log(MyReviewData?.page);
+    }
+  };
+  const handleDeleteReview = async (reviewId: number, imgSrc: string) => {
+    if (window.confirm('정말 삭제 하시겠습니까?')) {
+      const response = await deleteReview(reviewId, imgSrc);
+      if (response.statusCode === 200) {
+        handleMyReviewData();
+      }
     }
   };
 
   useEffect(() => {
     handleMyReviewData();
   }, [page]);
+
   return (
-    <div>
-      <h1>작성한 후기</h1>
-      <br />
-      {reviewList.map((myreview) => (
-        <MyReviewsItem key={myreview.id} myreview={myreview} />
-      ))}
-      <div className="lessons-page__pagination">
+    <div className="my-review">
+      <div className="my-review__review-list">
+        {reviews.map((review: ReviewType) => (
+          <ReviewItem
+            review={review}
+            key={review.id}
+            handleDeleteReview={handleDeleteReview}
+          />
+        ))}
+      </div>
+      <div className="my-review__pagination">
         <Pagination
           variant="outlined"
           count={count}
@@ -78,4 +71,5 @@ const MyReviewsPage = () => {
     </div>
   );
 };
+
 export default MyReviewsPage;
