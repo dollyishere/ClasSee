7e9422e114160b96927eb42a896b1bd23edcfaab
김진호 <wlsghsla94@naver.com>
2023-02-14 17:40:26 +0900

    Feat: '[FE] 메인페이지 강의 없음 카드 추가'
    [#400]

--- frontend/src/components/MainPage/MyAppliedLessonsMainpage.tsx
+++ frontend/src/components/MainPage/MyAppliedLessonsMainpage.tsx
@@ -5,12 +5,14 @@ import MyAppliedLessonCard from '../MyAppliedLessonCard';
 import useViewModel from '../../viewmodels/MainPageViewModel';
 import { LessonsResponse, Lesson } from '../../types/LessonsType';
 import privateInfoState from '../../models/PrivateInfoAtom';
+import Nolesson from './NoLesson';
 
 // 로그인이 되었을 때만 이 컴포넌트가 보여짐
 // 내가 신청한 강의를 get으로 api요청 보냄
 // 강의가 있으면 강의카드를 보여주고
 // 강의가 없다면(빈 배열이라면) 없음을 보여줌
 const MyAppliedLessonsMainpage = () => {
+  const message = '신청';
   const { getMyAppliedLessonsMainpage } = useViewModel();
   const [lessons, setLessons] = useState<Lesson[]>();
   const userInfo = useRecoilValue(privateInfoState);
@@ -20,8 +22,12 @@ const MyAppliedLessonsMainpage = () => {
     if (userInfo) {
       getMyAppliedLessonsMainpage(userInfo.email, 2, 0, 'TODO').then(
         (response: LessonsResponse) => {
-          console.log('내가 신청한 강의', response.lessonInfoList);
-          setLessons(response.lessonInfoList);
+          console.log('내가 신청한 강의', response.lessonInfoList.length);
+          if (response.lessonInfoList.length === 0) {
+            setLessons(undefined);
+          } else {
+            setLessons(response.lessonInfoList);
+          }
         },
       );
     }
@@ -35,7 +41,7 @@ const MyAppliedLessonsMainpage = () => {
             <MyAppliedLessonCard lesson={lesson} key={lesson.lessonId} />
           ))
         ) : (
-          <div>no Created</div>
+          <Nolesson message={message} />
         )}
       </div>
     </div>
--- frontend/src/components/MainPage/MyCreatedLessonsMainpage.tsx
+++ frontend/src/components/MainPage/MyCreatedLessonsMainpage.tsx
@@ -4,11 +4,15 @@ import LessonCard from '../LessonCard';
 import useViewModel from '../../viewmodels/MainPageViewModel';
 import { LessonsResponse, Lesson } from '../../types/LessonsType';
 import privateInfoState from '../../models/PrivateInfoAtom';
+import Nolesson from './NoLesson';
+
 // 로그인이 되었을 때만 이 컴포넌트가 보여짐
 // 내가 개설한 강의를 get으로 api요청 보냄
 // 강의가 있으면 강의카드를 보여주고
 // 강의가 없다면(빈 배열이라면) 없음을 보여줌
 const MyCreatedLessonsMainpage = () => {
+  const message = '개설';
+
   // 내가 개설한 강의 2개 가져오는 함수
   const { getMyCreatedLessonsMainpage } = useViewModel();
   const [lessons, setLessons] = useState<Lesson[]>();
@@ -19,8 +23,12 @@ const MyCreatedLessonsMainpage = () => {
     if (userInfo) {
       getMyCreatedLessonsMainpage(userInfo.email, 2, 0).then(
         (response: LessonsResponse) => {
-          console.log('내가 개설한 강의', response.lessonInfoList);
-          setLessons(response.lessonInfoList);
+          console.log('내가 개설한 강의', response.lessonInfoList.length);
+          if (response.lessonInfoList.length === 0) {
+            setLessons(undefined);
+          } else {
+            setLessons(response.lessonInfoList);
+          }
         },
       );
     }
@@ -29,15 +37,15 @@ const MyCreatedLessonsMainpage = () => {
   return (
     <div className="createlessons">
       <h1 className="createlessons__title"> 개설한 클래스 </h1>
-      {lessons ? (
-        <div className="createlessons__cards">
-          {lessons.map((lesson: Lesson) => (
+      <div className="createlessons__cards">
+        {lessons ? (
+          lessons.map((lesson: Lesson) => (
             <LessonCard lesson={lesson} key={lesson.lessonId} />
-          ))}
-        </div>
-      ) : (
-        <div>no Created</div>
-      )}
+          ))
+        ) : (
+          <Nolesson message={message} />
+        )}
+      </div>
     </div>
   );
 };
